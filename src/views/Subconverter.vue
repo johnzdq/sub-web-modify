f5e1c3dd88e128fbf170dd49b56a6769858539ca
johnzdq <johnzdq@5r5.me>
2025-12-25 18:49:49 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,6 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "5r5后端": "http://154.219.110.11:53500/version",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -1310,3 +1311,4 @@ export default {
   }
 };
 </script>
+
